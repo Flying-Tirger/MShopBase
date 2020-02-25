c3c050349804c967694189323519cb996605b4e3
Zhangbao <huawei@DESKTOP-E4U3GTO>
2020-02-25 22:38:40 +0800

    商品表与商品类别表

--- MShopBaseApi/Model/GoodsModel.cs
+++ MShopBaseApi/Model/GoodsModel.cs
@@ -5,7 +5,40 @@ using System.Threading.Tasks;
 
 namespace MShopBaseApi.Model
 {
+    /// <summary>
+    /// 商品表
+    /// </summary>
     public class GoodsModel
     {
+        public int Gid { get; set; }
+        /// <summary>
+        /// 商品编号
+        /// </summary>
+        public string  GHB { get; set; }
+        /// <summary>
+        /// 商品名称
+        /// </summary>
+        public string GName { get; set; }
+        /// <summary>
+        /// 商品价格
+        /// </summary>
+        public decimal GPrice { get; set; }
+        /// <summary>
+        /// 商品图片
+        /// </summary>
+        public string GImg { get; set; }
+        /// <summary>
+        /// 商品评分
+        /// </summary>
+        public float GGrade { get; set; }
+        /// <summary>
+        /// 商品销量
+        /// </summary>
+        public int GSales { get; set; }
+        /// <summary>
+        /// 外键 商品类别
+        /// </summary>
+        public int GTypeId { get; set; }
+
     }
 }
--- MShopBaseApi/Model/GoodsTypeModel.cs
+++ MShopBaseApi/Model/GoodsTypeModel.cs
@@ -5,7 +5,19 @@ using System.Threading.Tasks;
 
 namespace MShopBaseApi.Model
 {
+    /// <summary>
+    /// 商品类别表
+    /// </summary>
     public class GoodsTypeModel
     {
+        public int GoodsTypeId { get; set; }
+        /// <summary>
+        /// 类型名称
+        /// </summary>
+        public string GoodTypeName { get; set; }
+        /// <summary>
+        /// 是否启用
+        /// </summary>
+        public bool GTState { get; set; }
     }
 }

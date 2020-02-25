6e98d69ef6db9370e39ecdcf6ffc50f717eced26
1829575905@qq.com <1829575905@qq.com>
2020-02-25 20:37:30 +0800

    ysy

--- MShopBaseApi/Model/ExpressModel.cs
+++ MShopBaseApi/Model/ExpressModel.cs
@@ -5,7 +5,14 @@ using System.Threading.Tasks;
 
 namespace MShopBaseApi.Model
 {
+    /// <summary>
+    /// 物流表
+    /// </summary>
     public class ExpressModel
     {
+            public int ExpressId { get; set; }         //主键
+            public string ExpBH { get; set; }          //物流编号
+            public string ExpCompany { get; set; }     //公司名称
+            public string ExpInfo { get; set; }        //物流信息 
     }
 }

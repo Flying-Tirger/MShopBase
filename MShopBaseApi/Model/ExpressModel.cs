7ea88f2d6194fb769f4f5273c60da73273b45ef6
刘印 <1604120269@qq.com>
2020-02-25 20:48:48 +0800

    ly

--- MShopBaseApi/Model/ExpressModel.cs
+++ MShopBaseApi/Model/ExpressModel.cs
@@ -5,7 +5,35 @@ using System.Threading.Tasks;
 
 namespace MShopBaseApi.Model
 {
+    /// <summary>
+    /// 收货地址表
+    /// </summary>
     public class ExpressModel
     {
+
+        /// <summary>
+        /// 地址表Id
+        /// </summary>
+        public int PfId { get; set; }
+        /// <summary>
+        /// 联系人名称
+        /// </summary>
+        public string PfName { get; set; }
+        /// <summary>
+        ///全面详细地址
+        /// </summary>
+        public string PfAddres { get; set; }
+        /// <summary>
+        /// 手机号码
+        /// </summary>
+        public string PfPhone { get; set; }
+        /// <summary>
+        /// 是否启用
+        /// </summary>
+        public bool PfState { get; set; }
+        /// <summary>
+        /// 连接用户表
+        /// </summary>
+        public int UserId { get; set; }
     }
 }
--- MShopBaseApi/Model/FoormarkModel.cs
+++ MShopBaseApi/Model/FoormarkModel.cs
@@ -5,7 +5,23 @@ using System.Threading.Tasks;
 
 namespace MShopBaseApi.Model
 {
+    /// <summary>
+    /// 足迹表
+    /// </summary>
     public class FoormarkModel
     {
+        /// <summary>
+        /// 足迹表id
+        /// </summary>
+        public int FId { get; set; }
+        /// <summary>
+        /// 足迹日期
+        /// </summary>
+        public DateTime EDate { get; set; }
+        /// <summary>
+        /// 连接商品表
+        /// </summary>
+        public int GoodsId { get; set; }
+
     }
 }

373c9c999c1b392a373cb547f8a652ea871c835e
1829575905@qq.com <1829575905@qq.com>
2020-02-29 15:39:21 +0800

    ysy

--- MShopBaseApi/Controllers/OrderInfoController.cs
+++ MShopBaseApi/Controllers/OrderInfoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MShopBaseApi.Model;
 
 namespace MShopBaseApi.Controllers
 {
@@ -11,6 +12,34 @@ namespace MShopBaseApi.Controllers
     [ApiController]
     public class OrderInfoController : ControllerBase
     {
-      
-    }
+        /// <summary>
+        /// 显示
+        /// </summary>
+        /// <param name="OrderSid"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public List<OrderInfoModel> GetOrder(int OrderSid)
+        {
+            string sql = $"select o.OrderState,o.OrderBH,o.OrderNum,o.OrderTime,g.GPrice,g.GName,p.pfAddres,p.PfName,p.PfPhone,e.ExpCompany,e.ExpInfo from orderinfo as o join goods as g on o.GoodsId=g.`Gid `JOIN express as e ON o.ExpressId =e.ExpressId JOIN profilee p ON o.ProfileeId = p.PfId JOIN userinfo as u ON o.UserId = u.UId WHERE 1=1";
+            if (OrderSid != 0)
+            {
+                sql += $" and OrderState = {OrderSid}";
+            }
+            List<OrderInfoModel> list = DBHelper.GetToList<OrderInfoModel>(sql);
+            return list;
+        }   
+        /// <summary>
+        /// 添加
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public int PostOrder(OrderInfoModel model)
+        {
+            string sql = $"insert into orderinfo VALUES(DEFAULT(orderinfo.OId),'{model.OrderBH}',{model.GoodsId},{model.OrderNum},'{model.OrderTime}',{model.OrderState},{model.ExpressId},{model.ProfileeId},{model.UserId})";
+            int n = DBHelper.ExecuteNonQuery(sql);
+            return n;
+        }
+
+}
 }

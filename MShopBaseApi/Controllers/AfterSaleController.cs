cfecab3c45cf5bb5e438ef978c1108cc3b8ba691
陌离 <@DESKTOP-DG1OVRB>
2020-02-29 15:38:11 +0800

    售后添加删除

--- MShopBaseApi/Controllers/AfterSaleController.cs
+++ MShopBaseApi/Controllers/AfterSaleController.cs
@@ -4,25 +4,60 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-
+using MShopBaseApi.Model;
 namespace MShopBaseApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class AfterSaleController : ControllerBase
     {
-        public List<AfterOrderModel> Get(int userId, int id = 0)
+        public int MyProperty { get; set; }
+        [HttpGet]
+        /// <summary>
+        /// 显示，单项查看售后信息
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="Orderid"></param>
+        /// <param name="state"></param>
+        /// <returns></returns>
+        public List<AfterOrderModel> Get(int userId,int Orderid=0, int state = 0)
         {
             string sql = $"select g.GImg,g.GName,o.OrderNum,g.GPrice,o.OId,o.OrderBH,o.OrderTime,a.* from aftersale as a join orderinfo as o on a.AsId=o.OId join  goods  as g on g.`Gid `=o.GoodsId where o.UserId ={ userId}";
-            if (id != 0)
+            if (state != 0)
             {
-                sql += $" and AsState={id}";
+                sql += $" and AsState={state}";
+            }
+            if (Orderid!=0)
+            {
+                sql += $" and o.OId={Orderid}";
             }
             List<AfterOrderModel> list = DBHelper.GetToList<AfterOrderModel>(sql);
             return list;
         }
-
-
+        [HttpPost]
+        /// <summary>
+        /// 添加订单信息
+        /// </summary>
+        /// <param name="after"></param>
+        /// <returns></returns>
+        public int Post(AfterSaleModel after = null)
+        {
+            string sql = $"INSERT into aftersale(AsState,AsRemark,UserId,OrderId,ApplyTime,AsPhone,AsImg) VALUES({after.AsState}, '{ after.AsRemark}  ', {after.UserId}, {after.OrderId}, NOW(), '{after.AsPhone}', '{after.AsImg}')";
+            int n = DBHelper.ExecuteNonQuery(sql);
+            return n;
+        }
+        /// <summary>
+        /// 删除售后信息
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        public int Del(int id)
+        {
+            string sql = $"delete from aftersale where AsId={id}";
+            int n = DBHelper.ExecuteNonQuery(sql);
+            return n;
+        }
     }
     /// <summary>
     ///售后显示表
